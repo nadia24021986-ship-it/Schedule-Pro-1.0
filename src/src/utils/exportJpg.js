13869b04713d6ed4713ab71c9ae90fa80610e99c
nadia24021986-ship-it <nadia24021986@gmail.com>
2026-07-06 11:58:00 +0700

    Create exportJpg.js

---
+++ src/src/utils/exportJpg.js
@@ -0,0 +1,13 @@
+import html2canvas from 'html2canvas'
+
+export async function exportElementAsJpg(elementRef, filename) {
+  if (!elementRef.current) return
+  const canvas = await html2canvas(elementRef.current, {
+    scale: 2,
+    backgroundColor: '#ffffff',
+  })
+  const link = document.createElement('a')
+  link.download = `${filename}.jpg`
+  link.href = canvas.toDataURL('image/jpeg', 0.95)
+  link.click()
+}
