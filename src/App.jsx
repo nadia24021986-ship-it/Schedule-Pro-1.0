ffd5edfaf3719c70223529cf84b13744a6e414e2
nadia24021986-ship-it <nadia24021986@gmail.com>
2026-07-06 15:00:37 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import EmployeeView from './pages/EmployeeView.jsx'
-import AdminLogin from './pages/AdminLogin.jsx'
+import Home from './pages/Home.jsx'
 import AdminDashboard from './pages/AdminDashboard.jsx'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<EmployeeView />} />
-      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route path="/" element={<Home />} />
       <Route path="/admin" element={<AdminDashboard />} />
     </Routes>
   )
